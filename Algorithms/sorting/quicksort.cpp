9ec56461665ec06481e8a41bea329d63d2769999
Utkal97 <utkal.s15@iiits.in>
2019-12-14 13:27:54 +0530

    Algorithms

---
+++ Algorithms/sorting/quicksort.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+//Hoare's partition (not general partition) :
+int partition(int arr[], int pivotIndex, int lo, int hi)
+{
+	while(lo<hi) {
+
+		while(arr[lo] < arr[pivotIndex] && lo<hi) {
+			lo+=1;
+		}
+			
+		while(arr[hi] >= arr[pivotIndex] && lo<hi) {
+			hi-=1;
+		}
+
+		if(arr[lo] > arr[pivotIndex] && arr[hi] < arr[pivotIndex]) {
+			int temp=arr[lo];
+			arr[lo]=arr[hi];
+			arr[hi]=temp;
+		}
+	}
+		
+	return lo;
+}
+
+void quicksort(int arr[],int lo,int hi)
+{
+	if(lo==hi)
+		return;
+
+	else if(lo<hi) {
+	
+		int pivotIndex = hi;
+	
+		cout<<"placing "<<arr[pivotIndex];
+		
+		pivotIndex = partition(arr,pivotIndex,lo,hi);
+		
+		cout<<", its correct pos is "<<pivotIndex<<endl;
+		
+		int temp = arr[pivotIndex];
+		arr[pivotIndex] = arr[hi];
+		arr[hi] = temp;
+		
+		for(int i=lo;i<=hi;i++) {
+			printf("%d ",arr[i]);
+		}	cout<<endl;
+		quicksort(arr,lo,pivotIndex-1);
+		quicksort(arr,pivotIndex+1,hi);
+		return;
+	}
+	else
+		return;
+}
+
+int main()
+ {
+	int T=10;
+	//scanf("%d",&T);
+	//while(T--) {
+		int n=6;
+		//scanf("%d",&n);
+		int arr[]={7,4,2,3,5,6,213,54,33490,3489};
+	//	for(int i=0;i<n;i++) {
+	//		scanf("%d",&arr[i]);
+	//	}
+
+		quicksort(arr,0,10-1);
+		for(int i=0;i<10;i++) {
+			printf("%d ",arr[i]);
+		}
+	//}
+	return 0;
+}
